56b4bd24b5be4cd57f429ad12f96586c526bc85c
Risto Puolakainen <rpuolak@gmail.com>
2020-08-22 22:32:13 +0300

    Fix missing gun world appearance
    Make it act like other wands, with a little blue glow.

--- files/actions.lua
+++ files/actions.lua
@@ -6,15 +6,16 @@ MOD_PATH = "mods/portalgun/files/"
 table.insert( actions,
 {
   id                = "PORTAL",
-  name              = "Blue Portal",
-  description       = "Enables higher cognitive functions involving portals",
+  name              = "Portal battery pack",
+  description       = "It makes a low humming sound",
   sprite            = MOD_PATH .. "projectile/magazine.png",
   type              = ACTION_TYPE_PROJECTILE,
-  spawn_level       = "0,4,5,6",
+  spawn_level       = "6",
   spawn_probability = "1,1,1,1",
   price             = 600,
   mana              = 0,
   max_uses          = -1,
+  custom_xml_file   = "mods/portalgun/files/gun/blue_glow.xml",
   action = function()
     add_projectile(MOD_PATH .. "projectile/blue.xml")
     c.damage_critical_chance = 0
