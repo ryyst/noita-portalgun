19b95a930fc5a37fa449db60c0a4efb3b121c1e0
Risto Puolakainen <rpuolak@gmail.com>
2020-08-23 13:06:03 +0300

    Add spawnlock to init.lua

--- init.lua
+++ init.lua
@@ -13,17 +13,22 @@ function give_player_items(inventory, items)
 end
 
 
-function OnPlayerSpawned( player_entity )
+function OnPlayerSpawned(player)
+  -- Load portalgun only for first spawn
+  local LOAD_KEY = "PORTALGUN_FIRST_LOAD_DONE"
+  if GlobalsGetValue(LOAD_KEY, "0") == "1" then return end
+  GlobalsSetValue(LOAD_KEY, "1")
+  ---
+
   local inv_quick = EntityGetWithName("inventory_quick")
   local items_quick = {
     "mods/portalgun/files/gun/entity.xml",
   }
 
   give_player_items(inv_quick, items_quick)
-  
-  EntityAddComponent(player_entity, "LuaComponent",
-  {
-	script_source_file="mods/portalgun/files/gun/portalgun_activate.lua",
-	execute_every_n_frame="5"
+
+  EntityAddComponent(player, "LuaComponent", {
+    script_source_file="mods/portalgun/files/gun/portalgun_activate.lua",
+    execute_every_n_frame="5"
   })
 end
